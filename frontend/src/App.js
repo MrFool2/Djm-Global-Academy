bfdaa3717c26322c51263e8454937ef4846c8d09
Vishal <82036957+codevishal32@users.noreply.github.com>
2026-09-20 15:41:34 +0530

    This React js file for my Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hii this is my first react app. I am learning react from scratch. I will be making a lot of projects in react and will be sharing them with you all. So stay tuned for more updates.
+    </div>
+  );
+}
+
+export default App;
